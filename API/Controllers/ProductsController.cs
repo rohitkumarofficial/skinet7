bd7cb8bcf28025dd59e3589c214e2fa42148b414
Rohit Kumar <ROHITKUMARJAJORIYA@GMAIL.COM>
2023-09-14 14:43:11 +0530

    initial commit with API, Core, Infrastructure project

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+        public ProductsController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _storeContext.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
